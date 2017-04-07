f0f283126bd36bf38330cfffe648ba1a61fc1322
Jason Bustard <railstar@gmail.com>
2017-04-07 15:21:08 -0700

    BBuilding out forms for service data

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -14,16 +14,30 @@ $(document).ready(function(){
 		// }
     // });
 	
+	function selectSchool() {
+		
+	}
+	
 	$.getJSON("../json/data.json", function (data){
 		console.log(data);
 		$.each( data, function( key, val ) {
 			$("#SchoolTable").append(
 				"<div class='school'><h2 class='name'>"+data[key].name+"</h2><p><span class='type'>Type: "+data[key].type+"</span><br/><span class='address'>Address: "+data[key].address+"</span><br/><span class='city'>City: "+data[key].city+"</span><br/><span class='zipcode'>Zipcode: "+data[key].zipcode+"</span><br/><span class='phone'>Phone: "+data[key].phone+"</span></p></div>"
 			)
+			
+			
 		});
 	})
 	
-
+	$("#SchoolTable").on("click", ".name", function(){
+		let tag = $(this);
+		tag.parent(".school").toggleClass("selected");
+	});
+	
+	$("#SchoolTable").on("click", ".selected", function(){
+		let tag = $(this);
+		tag.removeClass("selected");
+	});
 	console.log(SCHOOLS);
 	
 
