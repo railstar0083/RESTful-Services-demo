148128537dcf64158b5f6d0fc5828237cdb54000
Jason Bustard <railstar@gmail.com>
2017-04-11 14:52:55 -0700

    Edit forms now auto-fill with selected data.

--- app/public/scripts/data.js
+++ app/public/scripts/data.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-	var SCHOOLS = new Array;
+	
 	
 	// $.ajax({
 		// url: "../json/data.json",
@@ -15,31 +15,42 @@ $(document).ready(function(){
     // });
 	
 	function selectSchool() {
+		var SCHOOLS = new Array;
 		$(".edit-school-forms").empty();
 		let numberSelected = 0;
-		numberSelected = $(".selected").length
-		console.log(numberSelected);
-		// $(".selected").each(function(){
-			// SCHOOLS.push(this);
-		// })
-		//console.log(SCHOOLS);
-		let i=1;
-		while(i <= numberSelected)
+		numberSelected = $(".selected").length;
+		$(".selected").each(function(){
+			let id = $(this).attr("id");
+			SCHOOLS.push(id);
+		})
+		console.log(SCHOOLS);
+		let i=0;
+		while(i < numberSelected)
 		{
 			$(".edit-school-forms").append(
-				"<div class='form-wrapper'><form method='post' action='/edit_school'><label>School Name</label><input type='text' name='school[name]' class='new-input name-input' /><label>Type</label><input type='text' name='school[type]' class='new-input type-input' placeholder='(ex. High School)'/><label>Address</label><input type='text' name='school[address]' class='new-input address-input' placeholder='(No P.O. Boxes)'/><label>City</label><input type='text' name='school[city]' class='new-input city-input' placeholder='' /><label>Zip Code</label><input type='text' name='school[zip]' class='new-input zip-input' placeholder='' /><label>Phone</label><input type='text' name='school[phone]' class='new-input phone-input' placeholder='optional' /></form></div>"
+				"<div class='form-wrapper' id='"+SCHOOLS[i]+"'><form method='post' action='/edit_school'><label>School Name</label><input type='text' name='school[name]' class='new-input name-input' /><label>Type</label><input type='text' name='school[type]' class='new-input type-input' placeholder='(ex. High School)'/><label>Address</label><input type='text' name='school[address]' class='new-input address-input' placeholder='(No P.O. Boxes)'/><label>City</label><input type='text' name='school[city]' class='new-input city-input' placeholder='' /><label>Zip Code</label><input type='text' name='school[zip]' class='new-input zip-input' placeholder='' /><label>Phone</label><input type='text' name='school[phone]' class='new-input phone-input' placeholder='optional' /></form></div>"
 			)
+			$(".form-wrapper#" + SCHOOLS[i] + " form .name-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .name").text());
+			$(".form-wrapper#" + SCHOOLS[i] + " form .type-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .type").text().replace('Type: ', ''));
+			$(".form-wrapper#" + SCHOOLS[i] + " form .address-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .address").text().replace('Address: ', ''));
+			$(".form-wrapper#" + SCHOOLS[i] + " form .city-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .city").text().replace('City: ', ''));
+			$(".form-wrapper#" + SCHOOLS[i] + " form .zip-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .zipcode").text().replace('Zipcode: ', ''));
+			$(".form-wrapper#" + SCHOOLS[i] + " form .phone-input").val($("#SchoolTable .school#" + SCHOOLS[i] + " .phone").text().replace('Phone: ', ''));
 			i++;
 		}
+		
 	}
 	
 	$.getJSON("../json/data.json", function (data){
-		console.log(data);
+		//console.log(data);
+		let school = 1;
 		$.each( data, function( key, val ) {
+			
 			$("#SchoolTable").append(
-				"<div class='school'><h2 class='name'>"+data[key].name+"</h2><p><span class='type'>Type: "+data[key].type+"</span><br/><span class='address'>Address: "+data[key].address+"</span><br/><span class='city'>City: "+data[key].city+"</span><br/><span class='zipcode'>Zipcode: "+data[key].zipcode+"</span><br/><span class='phone'>Phone: "+data[key].phone+"</span></p></div>"
+				"<div class='school' id='school"+school+"'><h2 class='name'>"+data[key].name+"</h2><p><span class='type'>Type: "+data[key].type+"</span><br/><span class='address'>Address: "+data[key].address+"</span><br/><span class='city'>City: "+data[key].city+"</span><br/><span class='zipcode'>Zipcode: "+data[key].zipcode+"</span><br/><span class='phone'>Phone: "+data[key].phone+"</span></p></div>"
 			)
 			
+			school++;
 			
 		});
 	})
@@ -76,7 +87,7 @@ $(document).ready(function(){
 		selectSchool();
 	});
 	
-	console.log(SCHOOLS);
+	//console.log(SCHOOLS);
 	
 
 	
